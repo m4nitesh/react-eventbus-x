2245b577760877d0b9c925292c27d4ed06de756f
m4nitesh <nitesh.kumar@shipsy.in>
2019-10-27 14:39:31 +0530

    added callback method arch

--- index.js
+++ index.js
@@ -4,19 +4,26 @@
 
 class EventBus {
 
-  constructor(){
+
+  constructor() {
     /**
      * Initialize and Attach Eventbus to window if not done
      */
+    if (!window.EventBus) {
+      window.EventBus = {}
+    }
   }
- 
+
   /**
    * Add to the list of subscribers, if already added reject and throw error
    * args - unique key and object
    * return type void
    */
-  static addSubscriber = (args) => {
-    
+  static addSubscriber = (Component) => {
+    if (!Component.prototype.getEvent) {
+      throw new Error('Callback method not added to Component')
+    }
+    window.EventBus[Component.name] = Component.prototype.getEvent;
   }
 
   /**
@@ -24,14 +31,16 @@ class EventBus {
    * args - unique key and object
    * return type - boolean
    */
-  static isSubscriberAttached = (args) => {
+  static isSubscriberAttached = (Component) => {
+    if (window.EventBus[Component.name] == '') {
 
+    }
   }
 
   /**
    * Remove from list of subscribers and if already added otherwise throw error
    */
-  static unsubscribe = (args) => {
+  static unsubscribe = (Component) => {
 
   }
 
@@ -39,8 +48,8 @@ class EventBus {
    * Checks if events bus attached to window if not throws error(internal error, constructor didn't work)
    * Finds in the list of subscribers and sends
   */
-  static sendEvent(args){
-
+  static sendEvent(key, val) {
+    Location
   }
 
 }
